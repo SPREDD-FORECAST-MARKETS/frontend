f88c2e252c8b7d7bb5c890f722c51a3c0442d3ba
shlok <shlokjagtap123@gmail.com>
2025-05-18 20:48:17 +0530

    Implemented Routes structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter,Route,Routes } from "react-router-dom"
+import LandingPage from "./pages/LandingPage"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="w-fuill h-screen bg-red-500">
+      <div className="">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/about" element={<div>About</div>} />
+            <Route path="/contact" element={<div>Contact</div>} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   )
